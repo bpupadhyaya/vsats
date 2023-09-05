52eaba43a1599245e88b3c0a0e404cde1bc3d593
bpupadhyaya <bpupadhyaya@gmail.com>
2023-09-04 21:55:07 -0700

    Wallet, work in progress.

---
+++ lab/bc-1-go/util/base58.go
@@ -0,0 +1,13 @@
+package util
+
+// Base58Encode encodes a byte array to Base58
+func Base58Encode(input []byte) []byte {
+	// TODO
+	return nil // TODO
+}
+
+// Base58Decode decodes Base58-encoded data
+func Base58Decode(input []byte) []byte {
+	// TODO
+	return nil // TODO
+}
--- lab/bc-1-go/wallet/wallet.go
+++ lab/bc-1-go/wallet/wallet.go
@@ -1,3 +1,68 @@
 package wallet
 
+import (
+	"b-1-go/util"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
+)
 
+const version = byte(0x00)
+const addressChecksumLen = 4
+
+// Wallet stores private and public keys
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+// NewWallet function creates and returns a Wallet
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+	wallet := Wallet{private, public}
+	return &wallet
+}
+
+// GetAddress returns wallet address
+func (w Wallet) GetAddress() []byte {
+	publicKeyHash := HashPublicKey(w.PublicKey)
+	versionedPayload := append([]byte{version}, publicKeyHash...)
+	checkSum := CheckSum(versionedPayload)
+	fullPayload := append(versionedPayload, checkSum...)
+	address := util.Base58Encode(fullPayload)
+
+	return address
+}
+
+// HashPublicKey hashes public key
+func HashPublicKey(publicKey []byte) []byte {
+	// TODO
+
+	return nil // TODO
+}
+
+// ValidateAddress checks if address is valid
+func ValidateAddress(address string) bool {
+	// TODO
+
+	return false // TODO
+}
+
+func CheckSum(payload []byte) []byte {
+	// TODO
+
+	return nil // TODO
+}
+
+// newKeyPair generates private-public key pair
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P521()
+	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	publicKey := append(privateKey.PublicKey.X.Bytes(), privateKey.PublicKey.Y.Bytes()...)
+
+	return *privateKey, publicKey
+}
