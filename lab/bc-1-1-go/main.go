3eb9b0489e9ecfa2471968f2680014b7d6362c22
bpupadhyaya <bpupadhyaya@gmail.com>
2023-09-18 15:47:59 -0700

    Basic blockchain.

--- lab/bc-1-1-go/main.go
+++ lab/bc-1-1-go/main.go
@@ -6,12 +6,12 @@ func main() {
 	blockChain := NewBlockchain()
 	blockChain.AddBlock("My first BTC send, 0.5BTC")
 	blockChain.AddBlock("My second BTC send, 1BTC")
-	var i int = 1
-	for _, block := range blockChain.blocks {
-		fmt.Printf("count: %d\n", i)
+
+	for j, block := range blockChain.blocks {
+		fmt.Printf("count: %d\n", j)
 		fmt.Printf("Previous hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		i++
+
 	}
 }
